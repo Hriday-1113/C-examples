526ee5f15be0fba75059a389379260c1ded9da47
Hriday-1113 <120484826+Hriday-1113@users.noreply.github.com>
2023-01-16 18:57:26 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int xstrlen(char*m){
+    int length=0;
+    while(*m!='\0'){
+      length++;
+   m++;
+    }
+    return length;
+}
+int main(){
+  char arr[1000];
+  printf("Enter any string :- ");
+  scanf("%[^\n]s",arr);
+      int len;
+      len=xstrlen(&arr[0]);
+      printf("The string length is %d",len);
+}
