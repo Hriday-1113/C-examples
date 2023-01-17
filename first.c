562769f36c940d76cfead8d8ca0fb6dabbb9569d
Hriday-1113 <120484826+Hriday-1113@users.noreply.github.com>
2023-01-17 19:09:41 +0530

    Add files via upload

--- first.c
+++ first.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
-void xstrcat(char*m,char*n,int lensource){ //temp,source
+void xstrcat(char*m,char*n,int lentemp){ 
 
  while(*n!='\0'){
- *(m+lensource)=*n;
-  lensource++;
+ *(m+lentemp)=*n;
+  lentemp++;
   n++;
- }
-*(m+lensource)='\0';
+                }
+*(m+lentemp)='\0';
 
 }
 
 int main(){
 char source[7]="Folks!";
 char temp[30]="Hello";
+
   xstrcat(temp,source,5);
  printf("%s",temp);
   return 0;
