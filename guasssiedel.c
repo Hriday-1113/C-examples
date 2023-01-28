219e356c2335879ef1ce84179cdccc8b725c4d87
Hriday-1113 <120484826+Hriday-1113@users.noreply.github.com>
2023-01-28 11:06:13 +0530

    Update guasssiedel.c

--- guasssiedel.c
+++ guasssiedel.c
@@ -13,7 +13,7 @@ int main(){
  
                        };
    if(arr[0][0]<arr[0][1]+arr[0][2] || arr[1][1]<arr[1][0]+arr[1][2] || arr[2][2]<arr[2][1]+arr[2][0]){
-  printf("Jacobian method not applicable");
+  printf("Guass siedel method not applicable");
   goto reenter;
                      } 
  
